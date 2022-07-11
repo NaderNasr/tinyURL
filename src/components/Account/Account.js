02cccfc241ebf322a17956a5c4bbceb37e184c74
NaderNasr <nadernasr7@gmail.com>
2022-07-11 11:28:03 -0400

    created the account toolbar

--- src/components/Account/Account.js
+++ src/components/Account/Account.js
@@ -1,9 +1,19 @@
 import React from 'react'
-import { Typography } from "@material-ui/core"
+import { Typography, AppBar, Toolbar, Button, Box } from "@material-ui/core"
 
 const Account = () => {
   return (
-    <Typography>Account</Typography>
+    <>
+      <AppBar>
+        <Toolbar>
+          <Typography variant='h6'>Tiny</Typography>
+          <Box ml='auto'>
+            <Button color='inherit'>Links</Button>
+            <Button color='inherit'>Logout</Button>
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </>
   )
 }
 
