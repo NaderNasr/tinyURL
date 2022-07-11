91af7b9ec07b4d0461051ff413ed55d085bf5d96
NaderNasr <nadernasr7@gmail.com>
2022-07-11 12:46:58 -0400

    creating navbar component

---
+++ src/components/Account/NavBar.jsx
@@ -0,0 +1,20 @@
+import { AppBar, Box, Button, Toolbar, Typography } from '@material-ui/core'
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <>
+      <AppBar position='static'>
+        <Toolbar>
+          <Typography variant='h6'>Tiny</Typography>
+          <Box ml='auto'>
+            <Button color='inherit'>Links</Button>
+            <Button color='inherit'>Logout</Button>
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </>
+  )
+}
+
+export default NavBar
