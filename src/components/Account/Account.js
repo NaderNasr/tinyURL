1f65bf1395368e4edb42ff19ea06cb19258170ce
NaderNasr <nadernasr7@gmail.com>
2022-07-11 12:46:43 -0400

    adding buttons and title

--- src/components/Account/Account.js
+++ src/components/Account/Account.js
@@ -1,18 +1,25 @@
 import React from 'react'
-import { Typography, AppBar, Toolbar, Button, Box } from "@material-ui/core"
+import { Box, Button, Grid, Typography } from "@material-ui/core"
+import NavBar from './NavBar'
 
 const Account = () => {
   return (
     <>
-      <AppBar>
-        <Toolbar>
-          <Typography variant='h6'>Tiny</Typography>
-          <Box ml='auto'>
-            <Button color='inherit'>Links</Button>
-            <Button color='inherit'>Logout</Button>
-          </Box>
-        </Toolbar>
-      </AppBar>
+      <NavBar />
+      <Box mt={5}>
+        <Grid container justifyContent='center'>
+          <Grid item xs={8}>
+            <Box display='flex'>
+              <Box mr={3}>
+              <Typography variant='h4'>
+                Links
+              </Typography>
+              </Box>
+              <Button variant='contained' color='primary'>Create New Link</Button>
+            </Box>
+          </Grid>
+        </Grid>
+      </Box>
     </>
   )
 }
