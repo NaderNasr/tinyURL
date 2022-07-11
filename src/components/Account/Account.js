32ccfe9a1127f4ed29bcdb6b68a047bbd37b6a62
NaderNasr <nadernasr7@gmail.com>
2022-07-11 11:12:06 -0400

    create account component

---
+++ src/components/Account/Account.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Typography } from "@material-ui/core"
+
+const Account = () => {
+  return (
+    <Typography>Account</Typography>
+  )
+}
+
+export default Account
