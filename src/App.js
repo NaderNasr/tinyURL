9ce90538a04b61cfd104aad52eb41f0e8550f27e
NaderNasr <nadernasr7@gmail.com>
2022-07-11 12:46:21 -0400

    remove switch from import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Switch, Route } from "react-router-dom"
+import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Account from "./components/Account/Account"
 import Home from "./components/Home/Home"
 
