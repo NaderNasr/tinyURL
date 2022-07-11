e48359a6c443d1b5b642b0feed65a07e094b12fd
NaderNasr <nadernasr7@gmail.com>
2022-07-11 11:12:15 -0400

    create home component

---
+++ src/components/Home/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Typography } from "@material-ui/core"
+
+const Home = () => {
+  return (
+    <Typography>Home Component</Typography>
+  )
+}
+
+export default Home
