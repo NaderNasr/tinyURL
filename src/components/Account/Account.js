1ce943bf171c9e22d3a287c379d3afc004a647a2
NaderNasr <nadernasr7@gmail.com>
2022-07-12 08:52:57 -0400

    dummy data for link item

--- src/components/Account/Account.js
+++ src/components/Account/Account.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Box, Button, Grid, Typography } from "@material-ui/core"
 import NavBar from './NavBar'
+import LinkItem from './LinkItem'
+
+const data = [
+  {
+    id: '1a2b3c4d',
+    createdAt: new Date(),
+    name: 'Portfolio',
+    longURL: 'www.nadernasr.ca',
+    shortHash: '1d3AbC',
+    numOfClicks: 7
+  },
+]
 
 const Account = () => {
+
+  const [links, setLinks] = useState(data);
+
   return (
     <>
       <NavBar />
@@ -11,12 +26,19 @@ const Account = () => {
           <Grid item xs={8}>
             <Box display='flex'>
               <Box mr={3}>
-              <Typography variant='h4'>
-                Links
-              </Typography>
+                <Typography variant='h4'>
+                  Links
+                </Typography>
               </Box>
-              <Button variant='contained' color='primary'>Create New Link</Button>
+              <Button variant='contained' color='primary'>Shorten Link</Button>
             </Box>
+            {links.map(link =>
+              <LinkItem
+                key={link.id}
+                //faster way to send all props to linkItem component
+                {...link}
+              />
+            )}
           </Grid>
         </Grid>
       </Box>
