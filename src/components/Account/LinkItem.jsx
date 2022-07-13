9aaa0aef01629003163c21870e7584f888275362
NaderNasr <nadernasr7@gmail.com>
2022-07-13 17:41:01 -0400

    date format

--- src/components/Account/LinkItem.jsx
+++ src/components/Account/LinkItem.jsx
@@ -1,30 +1,35 @@
 import { Typography, Button, Box } from "@material-ui/core"
 import { BarChart } from "@material-ui/icons"
+import format from 'date-fns/format'
 
 
 
-const LinkItem = ({id, createdAt, name, longURL, shortHash, numOfClicks}) => {
+const LinkItem = ({ id, createdAt, name, longURL, shortHash, numOfClicks }) => {
   return (
-    <Box>
-      <Box>
-        <Typography>
-          createdAt: {`${createdAt}`}
-        </Typography>
-        <Typography variant="h5">
-          {name}
-        </Typography>
-        <Typography>
-          {longURL}
-        </Typography>
+    <Box display='flex'>
+      <Box display='flex' justifyContent='space-between' alignItems='center'>
         <Box>
-          <Typography>
-             {window.location.hostname}/{shortHash}
+          <Typography variant="overline">
+            created at: {format(createdAt, 'd MMM y, HH:mm')}
           </Typography>
-          <Button variant="outlined" size="small">Copy</Button>
+          <Box my={1}>
+            <Typography variant="h5">
+              {name}
+            </Typography>
+            <Typography>
+              {longURL}
+            </Typography>
+          </Box>
+          <Box mr={5} display='flex'>
+            <Typography>
+              {window.location.hostname}/{shortHash}
+            </Typography>
+            <Button variant="outlined" size="small">Copy</Button>
+          </Box>
         </Box>
         <Box display='flex'>
           <Typography>{numOfClicks}</Typography>
-          <BarChart/>
+          <BarChart />
         </Box>
         <Typography>Total Clicks</Typography>
       </Box>
