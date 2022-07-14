3991c1cfe80daa99884e83924eba661cdd07368e
NaderNasr <nadernasr7@gmail.com>
2022-07-14 17:56:55 -0400

    checking if user is authenticated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Account from "./components/Account/Account"
 import Home from "./components/Home/Home"
 
+import {auth} from './firebase'
+
 const App = () => {
+
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    auth.onAuthStateChanged((isAuthenticated) => {
+       setUser(isAuthenticated)
+    })
+  }, []);
+
+
   return (
     <Router>
       <Routes>
