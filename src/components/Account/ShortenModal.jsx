44ca0cf27875235b7b535ce806fc0f00aba95e9d
NaderNasr <nadernasr7@gmail.com>
2022-07-16 12:54:18 -0400

    modal for create short link

---
+++ src/components/Account/ShortenModal.jsx
@@ -0,0 +1,46 @@
+import { Dialog, DialogActions, DialogContent, DialogTitle, Box, Button, TextField, IconButton } from '@material-ui/core'
+import { Close } from '@material-ui/icons'
+import { useState } from 'react'
+
+
+
+const ShortenModal = () => {
+
+  const [form, setForm] = useState({
+    name: '',
+    longURL: ''
+  });
+
+  const handleChange = e => {
+    setForm(oldForm => ({
+      ...oldForm,
+      [e.target.name]: e.target.value
+    }))
+  }
+
+  return (
+    <Dialog open={true} fullWidth>
+      <DialogTitle>
+        <Box display='flex' alignItems='center' justifyContent='space-between'>
+        Shorten URL
+        <IconButton>
+          <Close/>
+        </IconButton>
+        </Box>
+      </DialogTitle>
+      <DialogContent>
+        <Box mb={3}>
+          <TextField value={form.name} name='name' onChange={handleChange} fullWidth variant='filled' label='Name' />
+        </Box>
+        <TextField value={form.longURL} name='longURL' onChange={handleChange} fullWidth variant='filled' label='Long URL' />
+      </DialogContent>
+      <DialogActions>
+        <Box mr={2} my={1}>
+          <Button onClick={() => console.log(form)} color='primary' variant='contained' disableElevation>Shorten URL</Button>
+        </Box>
+      </DialogActions>
+    </Dialog>
+  )
+}
+
+export default ShortenModal
