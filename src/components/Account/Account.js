7b761146cbcc7a79e3683eec51a3a93b886b50a7
NaderNasr <nadernasr7@gmail.com>
2022-07-22 00:54:22 -0400

    reading and creating data from firebase database

--- src/components/Account/Account.js
+++ src/components/Account/Account.js
@@ -1,64 +1,99 @@
-import React, { Fragment, useState } from 'react'
+import React, { Fragment, useEffect, useState } from 'react'
 import { Box, Button, Divider, Grid, Typography } from "@material-ui/core"
 import NavBar from './NavBar'
 import LinkItem from './LinkItem'
 import ShortenModal from './ShortenModal'
+import { db, auth } from '../../firebase'
+import { nanoid } from 'nanoid'
+import { collection, CollectionReference, doc, addDoc, getDocs } from "firebase/firestore";
 
-const data = [
-  {
-    id: '1a2b3c4d',
-    createdAt: new Date(),
-    name: 'Portfolio',
-    longURL: 'www.nadernasr.ca',
-    shortHash: '1d3AbC',
-    numOfClicks: 7
-  },
-  {
-    id: '1a2b332c4d',
-    createdAt: new Date(),
-    name: 'Portfolio',
-    longURL: 'www.nadernasr.ca',
-    shortHash: '1d3AbC',
-    numOfClicks: 7
-  },
-]
 
 const Account = () => {
 
-  const [links, setLinks] = useState(data);
+  const [links, setLinks] = useState('');
+  const [newName, setName] = useState('');
+
+  const [users, setUsers] = useState([]);
+
   const [openModal, setOpenModal] = useState(false)
 
-  return (
-    <>
-      <NavBar />
-      {openModal ? <ShortenModal openModal={openModal} handleClose={() => setOpenModal(false)}/> : <></>}
-      <Box mt={5}>
-        <Grid container justifyContent='center'>
-          <Grid item xs={8}>
-            <Box display='flex' mb={5}>
-              <Box mr={3}>
-                <Typography variant='h4'>
-                  Links
-                </Typography>
-              </Box>
-              <Button variant='contained' color='primary' onClick={() => setOpenModal(true)}>Shorten Link</Button>
-            </Box>
-            {links.map(link =>
-              <Fragment key={link.id}>
-                <LinkItem
-                  //faster way to send all props to linkItem component
-                  {...link}
-                />
-                <Box my={5}>
-                <Divider />
+  const collectionReference = collection(db, 'users');
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const data = await getDocs(collectionReference);
+      setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+    getUsers();
+  }, [])
+
+    const createShortLink = async (name, longURL) => {
+
+      const userId = auth.currentUser.uid
+      setLinks([
+        {
+          name, longURL,
+          createdAt: Date.now(),
+          shortHash: nanoid(7),
+          numOfClicks: 0,
+          currentUser: userId
+        }
+      ])
+      await addDoc(collectionReference, { name: newName, longURL: links })
+      // const res = await db
+      //   .collection('users')
+      //   .doc(auth.currentUser.uid)
+      //   .collection('links')
+      //   .add(link)
+
+      // console.log()
+
+      // addDoc(collectionReference, links)
+      console.log(links)
+
+      setOpenModal(true)
+
+    }
+
+
+    console.log('users', users)
+
+    return (
+      <>
+        <NavBar />
+        {openModal ? <ShortenModal
+          createShortLink={createShortLink}
+          openModal={openModal}
+          setLinks={setLinks}
+          setName={setName}
+          handleClose={() => setOpenModal(false)} /> : <></>}
+        <Box mt={5}>
+          <Grid container justifyContent='center'>
+            <Grid item xs={8}>
+              <Box display='flex' mb={5}>
+                <Box mr={3}>
+                  <Typography variant='h4'>
+                    Links
+                  </Typography>
                 </Box>
-              </Fragment>
-            )}
+                <Button variant='contained' color='primary' onClick={() => setOpenModal(true)}>Shorten Link</Button>
+              </Box>
+              {users.map(link =>
+                <Fragment key={link.id}>
+                  <LinkItem
+                    //faster way to send all props to linkItem component
+                    {...link}
+                  />
+                  <Box my={5}>
+                    <Divider />
+                  </Box>
+                </Fragment>
+              )}
+            </Grid>
           </Grid>
-        </Grid>
-      </Box>
-    </>
-  )
-}
+        </Box>
+      </>
+    )
+  }
 
 export default Account
