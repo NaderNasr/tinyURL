242f83bcd82d1bfb58538b628190c89bb956e30c
NaderNasr <nadernasr7@gmail.com>
2022-07-25 13:11:30 -0400

    create link firestore function

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,31 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin")
+
+admin.initializeApp()
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  functions.logger.info("Hello logs!", {structuredData: true});
+  response.send("Hello from Firebase!");
+});
+
+exports.createLink = functions.firestore
+  .document('users/{users}')
+  .onCreate((snapshot, context) => {
+    console.log(snapshot.data())
+    const {users} = context.params
+    const {longURL, shortHash, currentUser, name} = snapshot.data()
+
+
+    return admin.firestore().doc(`users/${users}`).set({
+      currentUser,
+      longURL,
+      shortHash,
+      name
+    })
+
+
+    // return Promise.resolve()
+  })
