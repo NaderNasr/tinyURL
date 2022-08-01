e6a5b155cee2f1452b6dd09f617802bc305bc568
NaderNasr <nadernasr7@gmail.com>
2022-08-01 16:19:01 -0400

    check if url is valid

--- src/components/Account/ShortenModal.jsx
+++ src/components/Account/ShortenModal.jsx
@@ -3,13 +3,13 @@ import { Close } from '@material-ui/icons'
 import { useState } from 'react'
 
 
-
 const ShortenModal = ({ handleClose, createShortLink, setName, setLinks }) => {
 
   const [form, setForm] = useState({
     name: '',
     longURL: ''
   });
+
 
   const handleChange = e => {
     setForm(oldForm => ({
@@ -21,6 +21,13 @@ const ShortenModal = ({ handleClose, createShortLink, setName, setLinks }) => {
   const handleSubmit = () => {
     createShortLink()
   }
+
+  const isValidUrl = (string) => {
+    const matchPattern = /^(?:\w+:)?\/\/([^\s\.]+\.\S{2}|localhost[\:?\d]*)\S*$/;
+    return matchPattern.test(string);
+  }
+
+  const checkLink = url => isValidUrl(url) ? true : false
 
   return (
     <Dialog open={true} fullWidth>
@@ -36,11 +43,12 @@ const ShortenModal = ({ handleClose, createShortLink, setName, setLinks }) => {
         <Box mb={3}>
           <TextField value={setName(form.name)} name='name' onChange={handleChange} fullWidth variant='filled' label='Name' />
         </Box>
-        <TextField value={setLinks(form.longURL)} name='longURL' onChange={handleChange} fullWidth variant='filled' label='Long URL' />
+        <TextField value={setLinks(form.longURL)} name='longURL' onChange={handleChange} fullWidth variant='filled' label='Long URL' placeholder='http://'/>
       </DialogContent>
       <DialogActions>
         <Box mr={2} my={1}>
-          <Button onClick={handleSubmit} color='primary' variant='contained' disableElevation>Shorten URL</Button>
+          <Button onClick={handleSubmit} disabled={!checkLink(form.longURL)} color='primary' variant='contained' disableElevation>Shorten URL</Button>
+
         </Box>
       </DialogActions>
     </Dialog>
