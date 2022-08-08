1a9fd79af522d6d93e3af789a020d5c3e0e14c94
NaderNasr <nadernasr7@gmail.com>
2022-08-08 10:20:56 -0400

    alerts on user auth error

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,18 @@
 import React, { useState } from 'react';
-import { Typography, TextField, Button } from "@material-ui/core";
+import { Typography, TextField, Button, Snackbar } from "@material-ui/core";
 import { auth } from '../../firebase';
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import { createUserWithEmailAndPassword, sendPasswordResetEmail, signInWithEmailAndPassword } from 'firebase/auth';
 import './Home.css';
+import NavBar from '../Account/NavBar';
+import Alert from '@mui/material/Alert';
+import AlertTitle from '@mui/material/AlertTitle';
+import { Box, Collapse, IconButton } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
 
 const Home = () => {
+
+  const [logError, setLogError] = useState('')
+  const [open, setOpen] = useState(true)
 
   const [form, setForm] = useState({
     email: '',
@@ -20,22 +28,65 @@ const Home = () => {
 
   const handleSignUp = async () => {
     await createUserWithEmailAndPassword(auth, form.email, form.password).catch((error) => {
-      alert(`Sign up error ${error}`);
+      setOpen(true);
+      setLogError('Sign up - Email already in use');
     });
   }
+
 
   const handleSignIn = async () => {
     await signInWithEmailAndPassword(auth, form.email, form.password).catch((error) => {
-      alert(`Sign up error ${error}`);
+      setOpen(true);
+      setLogError('Sign in - Email or Password is incorrect');
     });
   }
 
+  // const handleResetPassword = async () => {
+  //   await sendPasswordResetEmail(auth, form.email)
+  // .then(() => {
+  //   console.log('password reset sent')
+  // })
+  // .catch((error) => {
+  //   const errorCode = error.code;
+  //   const errorMessage = error.message;
+  //   // ..
+  //   console.log('errorMessage:', errorMessage + 'errorCode:', errorCode)
+  // });
+  // }
+
   return (
-    <div
-      style={{ margin: '5em' }}
-    >
-      <div>
-      <Typography>TinyURL</Typography>
+    <div>
+      <NavBar>
+        <Typography>TinyURL</Typography>
+      </NavBar>
+      <div style={{ margin: '5em' }}>
+        {/* <Snackbar open={open} autoHideDuration={2200} onClose={() => setOpen(false)}> */}
+        {logError.length > 0 ?
+          <Collapse in={open}>
+            <Alert
+              variant="filled" severity="error"
+              action={
+                <IconButton
+                  aria-label="close"
+                  color="inherit"
+                  size="small"
+                  onClick={() => {
+                    setOpen(false);
+                  }}
+                >
+                  <CloseIcon fontSize="inherit" />
+                </IconButton>
+              }
+              sx={{ mb: 2 }}
+            >
+              <AlertTitle>Error</AlertTitle>
+              <strong>{logError}</strong>
+            </Alert>
+          </Collapse>
+          :
+          <></>
+        }
+        {/* </Snackbar> */}
 
         <TextField
           value={form.email}
@@ -62,7 +113,13 @@ const Home = () => {
           style={{ marginTop: '2em', display: 'flex', justifyContent: 'space-between' }}
         >
           <Button onClick={handleSignIn}>Sign In</Button>
+          {/* <Button onClick={handleResetPassword}>Forgot Password?</Button> */}
+          <Box display='flex'>
+          <p>
+            Not a member?
+          </p>
           <Button onClick={handleSignUp}>Sign Up</Button>
+          </Box>
         </div>
       </div>
 
